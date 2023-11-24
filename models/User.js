4149cedde429f6a773bdd27c131fa2c4bafd8ebd
jgooday48 <joelgooday48@gmail.com>
2023-11-24 12:28:27 +0000

    edit User model

--- models/User.js
+++ models/User.js
@@ -2,15 +2,17 @@ const db = require('../database/connect');
 
 class User {
 
-    constructor({ user_id, username, password, is_admin }) {
-        this.id = user_id;
-        this.username = username;
-        this.password = password;
-        this.isAdmin = is_admin;
+    constructor(data) {
+        this.id = data.user_id;
+        this.email = data.email;
+        this.password = data.password;
+        this.name = data.name
+        this.surname = data.surname
+        this.isAdmin = data.is_admin;
     }
 
     static async getOneById(id) {
-        const response = await db.query("SELECT * FROM user_account WHERE user_id = $1", [id]);
+        const response = await db.query("SELECT * FROM user_account WHERE email = $1", [id]);
         if (response.rows.length != 1) {
             throw new Error("Unable to locate user.");
         }
@@ -18,7 +20,7 @@ class User {
     }
 
     static async getOneByUsername(username) {
-        const response = await db.query("SELECT * FROM user_account WHERE username = $1", [username]);
+        const response = await db.query("SELECT * FROM user_account WHERE email = $1", [username]);
         if (response.rows.length != 1) {
             throw new Error("Unable to locate user.");
         }
@@ -26,9 +28,9 @@ class User {
     }
 
     static async create(data) {
-        const { username, password, isAdmin } = data;
-        let response = await db.query("INSERT INTO user_account (username, password) VALUES ($1, $2) RETURNING user_id;",
-            [username, password]);
+        const { username, password, name, surname } = data;
+        let response = await db.query("INSERT INTO user_account (email, password,name, surname) VALUES ($1, $2) RETURNING user_id;",
+            [username, password, name, surname]);
         const newId = response.rows[0].user_id;
         const newUser = await User.getOneById(newId);
         return newUser;
