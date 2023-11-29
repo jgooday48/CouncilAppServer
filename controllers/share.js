939e55e18cc99f102379137c248f14ea99404fe3
jgooday48 <joelgooday48@gmail.com>
2023-11-29 08:50:48 +0000

    allow only users to edit their own posts

--- controllers/share.js
+++ controllers/share.js
@@ -34,20 +34,37 @@ async function show (req, res) {
 };
 
 
-async function update(req,res) {
+
+async function update(req, res) {
     try {
         const id = parseInt(req.params.id)
         const data = req.body
-        const postToUpdate = await Share.getOneById(id)
-        const updatedPost = await postToUpdate.update(data)
-        
-        res.status(200).send(updatedPost)
-        
+        const userToken = req.headers['authorization'];
+        const token = await Token.getOneByToken(userToken);
+        const user = await User.getOneById(token.user_id);
+        const post = await Share.getOneById(id);
+
+        console.log('post.user_id:', post.user_id);
+        console.log('user.id:', user.id);
+
+        if (post.user_id === user.id) {
+            const postToUpdate = await Share.getOneById(id)
+            console.log('Updating post:', postToUpdate);
+
+            const updatedPost = await postToUpdate.update(data)
+            console.log('Updated post:', updatedPost);
+
+            res.status(200).send(updatedPost)
+        } else {
+            res.status(404).json({ error: 'You cant edit this' });
+        }
     } catch (err) {
-        res.status(404).json({"error": err.message})
-        
+        console.error('Error updating post:', err);
+        res.status(404).json({ "error": err.message })
     }
 }
+
+
 
 async function destroy (req, res) {
     try {
@@ -58,15 +75,12 @@ async function destroy (req, res) {
         
         const post = await Share.getOneById(id);
 
-        // res.status(204).end();
 
         if (post.user_id === user.id) {
             const result = await post.destroy();
             res.status(204).end();
           } else {
-            res.status(403).json({
-              error: 'You cant delete this'
-            });
+            res.status(403).json({error: 'You cant delete this as you are not the user who posted this'});
           }
     } catch (err) {
         res.status(404).json({"error": err.message})
